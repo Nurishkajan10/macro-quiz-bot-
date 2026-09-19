6db674b249adb1c437442832999f48373a8ac3d9
Nurishkajan10 <nury.ekzekov@yandex.ru>
2026-09-19 11:58:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -115,7 +115,7 @@ async def send_daily_quiz(context: ContextTypes.DEFAULT_TYPE):
                 options=[opt[:100] for opt in q["options"]],
                 type="quiz",
                 correct_option_id=q["correct"],
-                is_anonymous=False,
+                is_anonymous=True,
                 explanation=q["explanation"][:200],
             )
             await asyncio.sleep(1)  # чтобы не словить flood limit
